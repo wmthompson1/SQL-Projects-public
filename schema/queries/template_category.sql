bad1ce80d7e42ad2b9095a2f232d1d1a70e49b9d
wmthompson1-skills <williamt@skillsInc.org>
2025-12-22 11:11:41 -0800

    upd prep setup

---
+++ schema/queries/template_category.sql
@@ -0,0 +1,11 @@
+-- Name: Query Title Here
+-- Description: What this query does
+SELECT 
+    column1,
+    column2
+FROM table_name
+WHERE condition;
+
+-- Name: Another Query
+-- Description: Another description
+SELECT * FROM another_table;
