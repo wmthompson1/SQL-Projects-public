a428c093dcba261a850f45ddf6616f6f56cc9598
wmthompson1-skills <williamt@skillsInc.org>
2025-12-31 11:20:44 -0800

    upd

--- schema/queries/template_category.sql
+++ schema/queries/template_category.sql
@@ -1,11 +1,20 @@
--- Name: Query Title Here
--- Description: What this query does
+-- Ground Truth SQL: Supplier Performance Queries
+-- Source: Flask Manufacturing App (LangChain Semantic Layer)
+-- Category: Template Category
+
+
+-- Name: Sample 1 query Title
+-- Description: Description 1 What this query does
 SELECT 
     column1,
     column2
 FROM table_name
-WHERE condition;
+join second_table ON table_name.id = second_table.fk_id
+WHERE condition = 123;
 
--- Name: Another Query
--- Description: Another description
-SELECT * FROM another_table;
+-- Name: Another Sample 2 Query Title
+-- Description: Description 2 What this query does
+SELECT * 
+FROM another_table
+left table_name on another_table.ref_id = table_name.id
+WHERE another_condition = 'value';
