6e81e602e6baccea23de26896827af0b8a122d3f
wmthompson1-skills <williamt@skillsInc.org>
2026-01-22 07:09:04 -0800

    upd cache

--- Utilities/SQLMesh/models/colors.sql
+++
@@ -1,8 +0,0 @@
-MODEL (
-    name colors, -- The name of the model
-    kind FULL    -- The materialization strategy (e.g., VIEW, INCREMENTAL_BY_TIME_RANGE, FULL)
-);
-
-SELECT
-    'Yellow' AS color,
-    CURRENT_TIMESTAMP AS created_at;
